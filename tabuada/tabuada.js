a2f184d31eb0e47c5e69f4fc3735f2545690c029
Carolaine Queiroz <carolaine1303.com@gmail.com>
2024-03-30 22:46:42 -0300

    tabuada atualizada

--- tabuada/tabuada.js
+++ tabuada/tabuada.js
@@ -1,7 +1,20 @@
 function tabuada() {
     let num =  document.getElementById('num1');
     let result = document.getElementById('resultado');
-    if (num.ariaValueMax.length == 0) {
-        console.log
+
+
+   
+    if (num?.value?.length == 0) {
+        alert('Por favor, digite um número');
+    } else {
+        var n = Number(num.value)
+        result.innerHTML = ''
+        for (i=1; i<=10; i++) {
+            let item = document.createElement("option")
+            item.text = `${n} x ${i} = ${n*i}`
+
+            result.appendChild(item)
+        }
     }
 }
+
