85922efb3847716e277f4b8648a43a25b5563725
Carolaine Queiroz <carolaine1303.com@gmail.com>
2024-03-30 21:12:37 -0300

    atualizaçoes

---
+++ tabuada/tabuada.js
@@ -0,0 +1,7 @@
+function tabuada() {
+    let num =  document.getElementById('num1');
+    let result = document.getElementById('resultado');
+    if (num.ariaValueMax.length == 0) {
+        console.log
+    }
+}
