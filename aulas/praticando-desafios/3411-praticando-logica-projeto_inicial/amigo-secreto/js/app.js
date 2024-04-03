6db26992af19371dab54080912ad100bef8d485e
Carolaine Queiroz <carolaine1303.com@gmail.com>
2024-04-02 22:22:36 -0300

    Update app.js

---
+++ aulas/praticando-desafios/3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -0,0 +1,31 @@
+let amigos = [];
+
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+    let listaAmigos = document.getElementById('lista-amigos');
+    amigos.push(amigo.value);
+
+    if (listaAmigos.textContent == '') {
+        listaAmigos.textContent = amigo.value;
+    } else {
+        listaAmigos.textContent = listaAmigos.textContent + ', ' + amigo.value;
+    }
+    amigo.value = '';
+    
+}
+
+function sortear() {
+    embaralha(amigos);
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
