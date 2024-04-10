ae73ead5311eca472421adcd4874636034fda789
Carolaine Queiroz <carolaine1303.com@gmail.com>
2024-04-10 13:27:56 -0300

    Create tipo-boolean.js

---
+++ nodeJS/tipo-boolean.js
@@ -0,0 +1,8 @@
+const estudante = 'Erik';
+const estaAprovado = 'true';
+
+if (estaAprovado === true) {
+    console.log('Parabéns, você passou');
+} else {
+    console.log('Você está reprovado');
+}
