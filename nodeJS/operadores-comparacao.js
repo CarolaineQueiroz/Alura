c202ec3d95947f220f29843468f573d12bb0b2a2
Carolaine Queiroz <carolaine1303.com@gmail.com>
2024-04-12 15:15:50 -0300

    atualizações

---
+++ nodeJS/operadores-comparacao.js
@@ -0,0 +1,46 @@
+const estaAprovado = true;
+
+if (estaAprovado === true){
+    console.log('aprovado');
+}
+
+if ('0' === 0) {
+    console.log('Passou na comparação');
+} else {
+    console.log('Não passou na comparação');
+}
+
+const idadeMinima = 18;
+const idadeEstudante = 16;
+
+if (idadeEstudante >= idadeMinima) {
+    console.log('Não precisa de autorização');
+} else {
+    console.log('precisa de autorização');
+}
+
+
+
+/* let valor1 = '5';
+let valor2 = 5;
+
+let resultado1 = valor1 + valor2;
+let resultado2;
+
+if (valor1 === valor2) {
+ resultado2 = 'Os valores são iguais.';
+} else {
+ resultado2 = 'Os valores são diferentes.';
+}
+
+console.log('Resultado 1:', resultado1);
+console.log('Resultado 2:', resultado2); */
+
+/* const numero = 10;
+const resultado1 = (numero > 5 && numero < 15);
+const resultado2 = (numero === 10 || numero > 20);
+const resultado3 = !(numero < 5);
+
+console.log("Resultado 1:", resultado1);
+console.log("Resultado 2:", resultado2);
+console.log("Resultado 3:", resultado3); */
